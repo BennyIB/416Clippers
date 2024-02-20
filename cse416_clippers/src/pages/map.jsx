1fb9780db49b401eccd9a7df2cd0c0d32008ac7a
birajghimire <47505992+birajghimire@users.noreply.github.com>
2024-02-19 22:01:59 -0500

    adding modal

--- cse416_clippers/src/pages/map.jsx
+++ cse416_clippers/src/pages/map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Map, { Source, Layer } from 'react-map-gl';
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useParams } from 'react-router-dom';
@@ -18,6 +18,8 @@ const geojsonData = ArizonaAndIllinoisDistricts;
 
 const MapPage = () => {
     const map = useRef(null);
+    const [showModal, setShowModal] = useState(false);
+
     const accessToken = import.meta.env.VITE_MAPACCESS_TOKEN;
     const { state } = useParams();
     let lat = 39.8283;
@@ -57,6 +59,7 @@ const MapPage = () => {
     // }, [state]);
     const handleClick = (event) => {
       const { features } = event;
+      setShowModal(true);
       const clickedFeature = features && features.find(f => f.layer.id === layerStyle.id);
       console.log(event)
       if (clickedFeature) {
@@ -79,6 +82,32 @@ const MapPage = () => {
             interactiveLayerIds={['landuse_park']}
             // onLoad={handleLoad}
         >
+        {showModal && (
+                <div className="fixed inset-0 bg-white-600 bg-opacity-50 flex justify-center items-center z-50">
+                    <div 
+                        className="bg-white p-5 rounded-lg shadow-lg" 
+                        style={{
+                            width: '500px', // Fixed width
+                            height: '300px', // Fixed height
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between', // This ensures that the button aligns to the bottom
+                            overflow: 'auto' // Handles overflow content
+                        }}
+                    >
+                        <h2 className="text-lg text-black font-bold text-center">Arizona Data</h2>
+                        <p className="mb-4 text-black">MODALL</p>
+                        <div className="flex justify-center" >
+                            <button 
+                                onClick={() => setShowModal(false)} 
+                                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         <Source id="my-data" type="geojson" data={geojsonData}>
         <Layer {...layerStyle}/>
         </Source>
