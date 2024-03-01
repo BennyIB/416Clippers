58c00ffa00e25e56a8326bd68b1cc93eafa1dd0b
Brian <brianshao20@gmail.com>
2024-03-01 14:41:15 -0500

    Moved map controls and heatmap selection to far right. Added forward ref to get ref of child map component. Used useImperativeHandle to expose child map functions to parent.

--- frontend/src/pages/mapPage.jsx
+++ frontend/src/pages/mapPage.jsx
@@ -1,38 +1,51 @@
 import MyMap from "../components/map";
-import { useState } from "react";
-// import HeatMapSelection from "../components/HeatMapSelection";
-// import HeatMapLegend from "../components/HeatMapLegend";
-// import MapControl from "../components/MapControl";
+import { useState, useRef } from "react";
+import HeatMapSelection from "../components/HeatMapSelection";
+import HeatMapLegend from "../components/HeatMapLegend";
+import MapControl from "../components/MapControl";
 
-// const legendItems = [
-//     { color: '#ffffad', number: 10, value: '10%', textColor: '#000' },
-//     { color: '#f1e491', number: 20, value: '20%', textColor: '#000' },
-//     { color: '#e3ca77', number: 30, value: '30%', textColor: '#000' },
-//     { color: '#d5b05f', number: 40, value: '40%', textColor: '#000' },
-//     { color: '#c79649', number: 50, value: '50%', textColor: '#000' },
-//     { color: '#b97c35', number: 60, value: '60%', textColor: '#fff' },
-//     { color: '#aa6224', number: 70, value: '70%', textColor: '#fff' },
-//     { color: '#9a4716', number: 80, value: '80%', textColor: '#fff' },
-//     { color: '#8a2b0a', number: 90, value: '90%', textColor: '#fff' },
-//     { color: '#790000', number: 100, value: '100%', textColor: '#fff' },
-//   ];
+const legendItems = [
+    { color: '#ffffe0', number: 0, value: '0%', textColor: '#000' },
+    { color: '#ffffad', number: 10, value: '10%', textColor: '#000' },
+    { color: '#f1e491', number: 20, value: '20%', textColor: '#000' },
+    { color: '#e3ca77', number: 30, value: '30%', textColor: '#000' },
+    { color: '#d5b05f', number: 40, value: '40%', textColor: '#000' },
+    { color: '#c79649', number: 50, value: '50%', textColor: '#000' },
+    { color: '#b97c35', number: 60, value: '60%', textColor: '#fff' },
+    { color: '#aa6224', number: 70, value: '70%', textColor: '#fff' },
+    { color: '#9a4716', number: 80, value: '80%', textColor: '#fff' },
+    { color: '#8a2b0a', number: 90, value: '90%', textColor: '#fff' },
+    { color: '#790000', number: 100, value: '100%', textColor: '#fff' },
+];
 
 const MapPage = () => {
+    const mapRef = useRef(null);
     const [compareView, setCompareView] = useState(false);
     const [selectedHeatMap, setHeatMap] = useState("None");
+    const handleZoomIn = () => {
+        mapRef.current.zoomIn();
+    };
+
+    const handleZoomOut = () => {
+        mapRef.current.zoomOut();
+    };
+
+    const handleResetZoom = () => {
+        mapRef.current.resetZoom();
+    };
     return (
         <div className="relative w-full h-screen">
-            {/* {(selectedHeatMap !== "None" && selectedHeatMap !== "PoliticalPartyPreference") && <HeatMapLegend legendItems={legendItems} />}
+            {(selectedHeatMap !== "None" && selectedHeatMap !== "PoliticalPartyPreference") && <HeatMapLegend legendItems={legendItems} />}
             <HeatMapSelection selectedHeatMap={selectedHeatMap} setHeatMap={setHeatMap} />
-            <MapControl setCompareView={setCompareView} compareView={compareView} /> */}
+            <MapControl setCompareView={setCompareView} compareView={compareView} zoomIn={handleZoomIn} zoomOut={handleZoomOut} resetZoom={handleResetZoom} />
             <div className="flex w-full h-full">
                 <div className="flex-grow border-r border-gray-500">
-                    <MyMap setCompareView={setCompareView} selectedHeatMap={selectedHeatMap} setHeatMap={setHeatMap} compareView={compareView} left={true}/>
+                    <MyMap ref={mapRef} setCompareView={setCompareView} selectedHeatMap={selectedHeatMap} setHeatMap={setHeatMap} compareView={compareView} left={true} />
                 </div>
-                {compareView && 
-                (<div className="w-2/5">
-                    <MyMap compared={compareView} selectedHeatMap={selectedHeatMap} setHeatMap={setHeatMap}/>
-                </div>)}
+                {compareView &&
+                    (<div className="w-2/5">
+                        <MyMap compared={compareView} selectedHeatMap={selectedHeatMap} setHeatMap={setHeatMap} />
+                    </div>)}
             </div>
         </div>
     );
