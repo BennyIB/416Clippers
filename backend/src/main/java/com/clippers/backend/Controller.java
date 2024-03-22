515e4fd9b399751194f050d1ffc7676d82e765f5
skydaz123 <haowdy26@gmail.com>
2024-03-21 21:17:03 -0400

    Added functionality of pulling json data from resources folder

--- backend/src/main/java/com/clippers/backend/Controller.java
+++ backend/src/main/java/com/clippers/backend/Controller.java
@@ -1,13 +1,23 @@
-package main.java.com.clippers.backend; 
+package main.java.com.clippers.backend;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class MyController {
 
     @GetMapping("/Arizona_Illinois_Legislative_Districts")
-    public String helloWorld() {
-        return "Hello, World!";
+    public String getJson() throws IOException {
+        // Load the JSON file from the resources directory
+        ClassPathResource resource = new ClassPathResource("Arizona_Illinois_Boundary.json");
+        // Read the content of the JSON file
+        String jsonContent = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+        // Return the content as the response
+        return jsonContent;
     }
 }
