938af081fdd73a2baf17bca6cf92c2655eba0684
Brian <brianshao20@gmail.com>
2024-04-15 20:37:37 -0400

    Added generic Model

---
+++ backend/src/main/java/com/clippers/backend/model/MongoDocument.java
@@ -0,0 +1,33 @@
+package com.clippers.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public abstract class MongoDocument {
+    @Id
+    protected String id;
+    protected String type;
+
+    public MongoDocument(String id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+}
--- backend/src/main/java/com/clippers/backend/model/VoteShareData.java
+++ backend/src/main/java/com/clippers/backend/model/VoteShareData.java
@@ -1,52 +1,33 @@
 package com.clippers.backend.model;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
 
 import java.util.List;
 
 @Document(collection = "Arizona")
-public class VoteShareData {
-    @Id
-    private String id;
-    private String type;
+public class VoteShareData extends MongoDocument {
     @Field("democrat_points")
     private List<Object[]> democratPoints;
     @Field("republican_points")
     private List<Object[]> republicanPoints;
-    @Field("equation_democrat")  
-    private String equationDemocrat; 
-    @Field("equation_republican")  
-    private String equationRepublican;  
-
-    public VoteShareData() {}
+    @Field("equation_democrat")
+    private String equationDemocrat;
+    @Field("equation_republican")
+    private String equationRepublican;
 
     public VoteShareData(String id, String type, List<Object[]> democratPoints, List<Object[]> republicanPoints, String equationDemocrat, String equationRepublican) {
-        this.id = id;
-        this.type = type;
+        super(id, type);
         this.democratPoints = democratPoints;
         this.republicanPoints = republicanPoints;
         this.equationDemocrat = equationDemocrat;
         this.equationRepublican = equationRepublican;
     }
 
-
     // Getters and setters
-
-    public String getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
     public List<Object[]> getDemocratPoints() {
         return democratPoints;
     }
-
 
     public List<Object[]> getRepublicanPoints() {
         return republicanPoints;
@@ -59,8 +40,4 @@ public class VoteShareData {
     public String getEquationRepublican() {
         return equationRepublican;
     }
-    
-
-
-
 }
