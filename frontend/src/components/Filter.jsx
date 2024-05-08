3abf3b3fd6a8740d747c909672156c826455a9c4
Brian <brianshao20@gmail.com>
2024-05-08 19:25:07 -0400

    added reset button for the filter functionality

--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -1,33 +1,59 @@
+import React, { useState } from 'react';
+
 const Filter = (props) => {
-    
+    const [selectedParty, setSelectedParty] = useState('none');
+    const [selectedOperation, setSelectedOperation] = useState('AND');
+    const [selectedRace, setSelectedRace] = useState('none');
+
+    const resetFilters = () => {
+        setSelectedParty('none');
+        setSelectedOperation('AND');
+        setSelectedRace('none');
+        props.setSelectedParty('none');
+        props.setSelectedOperation('AND');
+        props.setSelectedRace('none');
+    };
+
     return (
         <div className="pt-2">
-            <label className="pr-1" for="filterparty">Filter:</label>
-            <select 
-                name="filterparty" 
-                id="filterparty" 
+            <label className="pr-1">Filter:</label>
+            <select
+                name="filterparty"
+                id="filterparty"
+                value={selectedParty}
                 className="select select-bordered select-sm bg-white"
-                onChange={(e) => props.setSelectedParty(e.target.value)}
+                onChange={(e) => {
+                    setSelectedParty(e.target.value);
+                    props.setSelectedParty(e.target.value);
+                }}
             >
                 <option value="none">None</option>
                 <option value="D">Democratic</option>
                 <option value="R">Republican</option>
                 <option value="other">Other</option>
             </select>
-            <select 
-                name="filterop" 
-                id="filterop" 
+            <select
+                name="filterop"
+                id="filterop"
+                value={selectedOperation}
                 className="select select-bordered select-sm bg-white"
-                onChange={(e) => props.setSelectedOperation(e.target.value)}
+                onChange={(e) => {
+                    setSelectedOperation(e.target.value);
+                    props.setSelectedOperation(e.target.value);
+                }}
             >
                 <option value="AND">AND</option>
                 <option value="OR">OR</option>
             </select>
-            <select 
-                name="filterrace" 
-                id="filterrace" 
+            <select
+                name="filterrace"
+                id="filterrace"
+                value={selectedRace}
                 className="select select-bordered select-sm bg-white"
-                onChange={(e) => props.setSelectedRace(e.target.value)}
+                onChange={(e) => {
+                    setSelectedRace(e.target.value);
+                    props.setSelectedRace(e.target.value);
+                }}
             >
                 <option value="none">None</option>
                 <option value="white">White</option>
@@ -35,7 +61,14 @@ const Filter = (props) => {
                 <option value="hispanic">Hispanic</option>
                 <option value="black">Black</option>
             </select>
+            <div
+                className="btn btn-ghost"
+                onClick={resetFilters}
+            >
+                <img className="w-4" src="/reset-icon.svg" alt="Reset"/>
+            </div>
         </div>
     );
-}
+};
+
 export default Filter;
